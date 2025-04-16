869ce6022bd5aa219f609ee557011d76e1356ffc
wycky1990 <wyclifffeomondi@gmail.com>
2025-04-16 16:47:27 -0700

    0x01. ES6 Promises

--- 0x01-ES6_promise/utils.js
+++ 0x01-ES6_promise/utils.js
@@ -8,7 +8,7 @@ export function uploadPhoto() {
 
 export function createUser() {
   return Promise.resolve({
-    firstName: 'Wycliffe',
-    lastName: 'Ochieng',
+    firstName: 'Guillaume',
+    lastName: 'Salva',
   });
 }
